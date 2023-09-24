4bc4bde08226b177735a9d232941cad1d5e144f7
Tigertron10 <andre.w.santos@outlook.com>
2023-09-23 23:09:09 -0300

    Last version to submit wee2 23-09_23

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -7,26 +7,26 @@ let fullName = "Andre William dos Santos";
 let currentYear = 2022;
 let profilePicture = "images/aboutme.jpg";
 
-
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('food');
+const foodElement = document.querySelector('#food');
 const yearElement = document.getElementById('#year');
 const imageElement = document.querySelector('picture img').src= profilePicture;
-imageElement.alt = "Profile Picture";
-
-
-
-/* Step 4 - Adding Content */
-nameElement.innerHTML ='<strong>' + fullName + '</strong>';
-yearElement.textContent = currentYear; 
-imageElement.setAttribute('scr',profilePicture);
-imageElement.setAttribute('alt','${profileImage}');
 
 /* Step 5 - Array */
 const favoritefood = ["Japanese Food", "Salad", "Cake", "Juice"];
+foodElement.innerHTML += `<br>${favoritefood}`;
+foodElement.textContent = favoritefood.join(", ");
 let singleFavoriteFood = "Cheese";
 favoritefood.push(singleFavoriteFood);
-foodElement.textContent = "123";
+foodElement.textContent = favoritefood.join(", ");
+favoritefood.shift();
+foodElement.innerHTML += `<br>${favoritefood}`;
+favoritefood.pop();
+foodElement.innerHTML += `<br>${favoritefood}`;
 
-
+/* Step 4 - Adding Content */
+nameElement.innerHTML =`<strong>${fullName}</strong>`;
+foodElement.innerHTML = foodElement.innerHTML;
+yearElement.textContent = currentYear; 
+imageElement.alt = fullName;
