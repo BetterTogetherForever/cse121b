4450d1b84bf704a426cf8892625910921dd5ac76
Tigertron10 <andre.w.santos@outlook.com>
2023-09-24 21:00:44 -0300

    task3

---
+++ scripts/w03-task.js
@@ -0,0 +1,42 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+
+/* Function Definition - Add Numbers */
+function add (number1,number2) {
+    //function body
+    return number1 + number2;
+    }
+    let addnumber1 = number(document.queryselector('#add1').value);
+    let addnumber2 = number(document.queryselector('#add2').value);
+    
+    function addnumbers(number1,number2) {
+    document.queryselector('#sum').value = add(addnumber1,addnumber2);
+    }
+    document.queryselector('#addNumbers').addeventlistener('click',addnumbers(addnumber1,addnumber2));
+    
+    /* Function Expression - Subtract Numbers */
+    
+    
+    /* Arrow Function - Multiply Numbers */
+    
+    
+    /* Open Function Use - Divide Numbers */
+    
+    
+    /* Decision Structure */
+    
+    
+    /* ARRAY METHODS - Functional Programming */
+    /* Output Source Array */
+    
+    /* Output Odds Only Array */
+    
+    /* Output Evens Only Array */
+    
+    /* Output Sum of Org. Array */
+    
+    /* Output Multiplied by 2 Array */
+    
+    /* Output Sum of Multiplied by 2 Array */
+    
